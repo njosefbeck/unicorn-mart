c68495a65ee41f5d55f4428655c63ee9949486cf
Nathan Beck <nathan.beck@fleishman.com>
2017-10-20 11:14:49 -0500

    Add contentful source plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+const tokens = require('./tokens');
+
 module.exports = {
   siteMetadata: {
     title: `Unicorn Mart`,
@@ -9,6 +11,13 @@ module.exports = {
       options: {
         trackingId: 'UA-44175001-4',
         anonymize: true
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: tokens.CONTENTFUL_SPACE_ID,
+        accessToken: tokens.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ],

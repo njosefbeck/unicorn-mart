9efd595a1c47ad27e4b902cce64fa9cbe1ce24e7
Nathan Beck <nathan.beck@fleishman.com>
2017-10-20 10:36:08 -0500

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Unicorn Mart`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-44175001-4',
+        anonymize: true
+      },
+    },
+  ],
 }
